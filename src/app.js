fa82f22661468ca279b5786629687b51abf67da5
Janinealves04 <Janinealves.sil@gmail.com>
2025-05-14 11:10:17 -0300

    Criação do metodo post para cadastrar um novo livro

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import express from "express";
 
 //express se torna responsavel pelo manejamento do servidor
 const app = express();
+//middleware: usado para executar a requisição e converte para json
+app.use(express.json()); 
+
 
 //simulação de base de dados(array de objetos)
 const livros = [
@@ -30,6 +33,11 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
+app.post ("/livros", (req, res) => {
+        livros.push(req.body); //objeto requisição que é recebio na função app.post
+        res.status(201).send("Livro cadastrado com sucesso");//criar algo com sucesso 201
+
+    })
 
 
 export default app;
